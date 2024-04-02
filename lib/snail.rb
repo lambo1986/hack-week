91ed571e57c865023682fb3de826a3e7c28c6c3d
Nathan Lambertson <lambertones@gmail.com>
2024-04-02 17:29:58 -0500

    Initial commit

---
+++ lib/snail.rb
@@ -0,0 +1,10 @@
+def snail(matrix)
+  part1 = matrix[0]
+  part2 = matrix[1][2]
+  part3 = matrix[2].reverse
+  part4 = matrix[1][0, 2]
+
+  whole = part1 << part2 << part3 << part4
+  
+  whole.flatten
+end
---
+++ spec/snail_spec.rb
@@ -0,0 +1,24 @@
+require "./lib/snail.rb"
+
+RSpec.describe "Snail Problems" do
+  describe "Snail" do
+    it "lists numbers from array matrix in a spiral" do
+      array_matrix1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    expect(snail(array_matrix1)).to eq([1, 2, 3, 6, 9, 8, 7, 4, 5])
+
+
+      array_matrix2 = [
+        [9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1]
+    ]
+  
+    expect(snail(array_matrix2)).to eq [9, 8, 7, 4, 1, 2, 3, 6, 5]
+    end
+  end
+end
